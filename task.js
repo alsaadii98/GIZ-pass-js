eae299849ccc8e91e248655ec1fa0d539ceaf23f
Ali H.Alsaadi <a.h.alsaady1998@gmail.com>
2021-09-30 15:50:28 +0300

    Ali H.Alsaadi task 1 done

--- task.js
+++ task.js
@@ -1,23 +1,17 @@
-let numbers = [
-    5,
-    8,
-    0,
-    1,
-    9,
-    11,
-    15,
-    16
-];
+const numbers = [5,8,0,1,9,11,15,16];
 
 console.log("Original numbers list: ", numbers)
-
-for(let i = 0 ; i < numbers.length ; i++)
+// The ASC order
+// The first iteration (i)
+for(let i = 0 ; i < numbers.length - 1 ; i++) // 1s needs to decrement the numbers.length by (1)
 {
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
+    // The second iteration (j)
+    for(let j = 0 ; j < numbers.length - 1 -i ; j++) // 2nd need to decrement the numbers.length by (1) and the (i) iteration by (1) 
     {
-        if(numbers[i] > numbers[j + 1])
+        // The if condition
+        if(numbers[j] > numbers[j + 1]) // The if condition it's wrong we should replace the (i) with (j)
         {
-            let temp = numbers[j];
+            let temp = numbers[j]; // To stor the value in it 
             numbers[j] = numbers[j + 1]
             numbers[j + 1] = temp;
         }
@@ -25,14 +19,16 @@ for(let i = 0 ; i < numbers.length ; i++)
 }
 
 console.log("Numbers list After sorting: ", numbers)
-
-for(let i = 0 ; i < numbers.length ; i++)
+// The DESC order
+// The first iteration (i)
+for(let i = 0 ; i < numbers.length +1 ; i++) // 1s needs to increment the numbers.length by (1)
 {
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
+    // The second iteration (j)
+    for(let j = 0 ; j < numbers.length + 1 +i ; j++) // 2nd need to increment the numbers.length by (1) and the (i) iteration by (1) 
     {
-        if(numbers[i] < numbers[j + 1])
+        if(numbers[j] < numbers[j + 1]) // The if condition it's wrong we should replace the (i) with (j)
         {
-            let temp = numbers[j];
+            let temp = numbers[j]; // To stor the value in it 
             numbers[j] = numbers[j + 1]
             numbers[j + 1] = temp;
         }
